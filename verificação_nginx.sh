5abcf3f9ac1a3e7bf2a66261198fdf45a3a386e0
nicoleevelyn99 <nicole-evelyn7@hotmail.com>
2024-11-07 16:29:29 -0300

    Update verificação_nginx.sh

--- verificação_nginx.sh
+++ verificação_nginx.sh
@@ -8,4 +8,4 @@ if systemctl is-active --quiet nginx; then
     echo "$(date '+%Y-%m-%d %H:%M:%S') - NGINX - Status: ONLINE - O serviço Nginx está rodando corretamente." >> ~/nginx_online_status.txt
 else
     echo "$(date '+%Y-%m-%d %H:%M:%S') - NGINX - Status: OFFLINE - O serviço Nginx não está rodando." >> ~/nginx_offline_status.txt
-fim
+fi
