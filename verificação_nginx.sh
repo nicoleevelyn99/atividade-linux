ee8c3df030878f7e61fc450a4abe6af820a214b8
nicoleevelyn99 <nicole-evelyn7@hotmail.com>
2024-11-07 15:28:40 -0300

    Update verificação_nginx.sh

--- verificação_nginx.sh
+++ verificação_nginx.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-# Defina o diretório onde os arquivos de saída serão armazenados
 OUTPUT_DIR=~/nginx_status_logs
-mkdir -p "$OUTPUT_DIR"  # Cria o diretório se não existir
+mkdir -p "$OUTPUT_DIR"  
 
-# Verifica o status do Nginx
+
 if systemctl is-active --quiet nginx; then
     echo "$(date '+%Y-%m-%d %H:%M:%S') - NGINX - Status: ONLINE - O serviço Nginx está rodando corretamente." >> ~/nginx_online_status.txt
 else
